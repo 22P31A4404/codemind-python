1de3e7f796ffbd9ba44f2675ecaf6f449df24c99
22P31A4404 <119731243+22P31A4404@users.noreply.github.com>
2023-06-05 09:52:57 +0530

    def is_palin(j):
        m = j
        s=0
        while m!=0:
            v = m%10
            s = s*10+v
            m = m//10
        if j==s:
            return j
    
    x = int(input())
    for i in range(x-1,1,-1):
        if is_palin(i):
            a = i
            break
    g = x+1
    while g!=0:
        if is_palin(g):
            b = g
            break
        g+=1
    if (x-a)<(b-x):
        print(a)
    elif (x-a)==(b-x):
        print(a,b)
    else:
        print(b)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,26 +1,28 @@
-def rev(x):
-    temp=x
-    rev=0
-    while(temp>0):
-       r=temp%10
-       rev=rev*10+r
-       temp//=10
-    return rev
-n=int(input())
-for i in range(n-1,0,-1):
-   if i==rev(i):
-      y=i
-      break
-i=n+1
-while(i>0):
-  if i==rev(i):
-      z=i
-      break
-  else:
-    i+=1
-if abs(n-y)<abs(n-z):
-   print(y)
-if abs(n-y)==abs(n-z):
-   print(y,z)
-if abs(n-y)>abs(n-z):
-   print(z)
+def is_palin(j): 
+
+    m = j
+    s=0
+    while m!=0:
+        v = m%10
+        s = s*10+v
+        m = m//10
+    if j==s:
+        return j
+
+x = int(input())
+for i in range(x-1,1,-1):
+    if is_palin(i):
+        a = i
+        break
+g = x+1
+while g!=0:
+    if is_palin(g):
+        b = g
+        break
+    g+=1
+if (x-a)<(b-x):
+    print(a)
+elif (x-a)==(b-x):
+    print(a,b)
+else:
+    print(b)
